611bc8c8981868b4cd02b1eef06d0cf0e67e4654
Dato Kevlishvili <dato.kevlishvili.1990@gmail.com>
2022-06-16 18:46:26 +0400

    music player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+// Css
 import './App.css';
+// Data
+import { music } from './data';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [paused, setPaused] = useState(true);
+
+  const audioRef = useRef();
+
+  const changeMusic = (direction) => {
+    if(direction === 'prew'){
+      if(count === 0){
+        setCount(music.length - 1);
+      }else{
+        setCount(count - 1);
+      }
+    }else{
+      if(count === music.length - 1){
+        setCount(0);
+      }else{
+        setCount(count + 1);
+      }
+    }
+    setPaused(false);
+  }
+
+  const handlePause = () => {
+    setPaused(!paused);
+    if(paused){
+      audioRef.current.play();
+    }else{
+      audioRef.current.pause();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <audio className='audio-player' ref={audioRef} src={music[count].src} controls autoPlay></audio>
+      <div className='player'>
+        <div className='img-div'>
+          <img src={music[count].img} alt='song-img' />
+        </div>
+        <div className='details'>
+          <div className='name'>
+            <p>Name</p>
+            <h4>{music[count].name}</h4>
+          </div>
+          <div className='author'>
+            <p>Author</p>
+            <h4>{music[count].author}</h4>
+          </div>
+        </div>
+        <div className='controls'>          
+          <button onClick={() => changeMusic('prew')}>
+            <i className="fa-solid fa-angle-left"></i>
+          </button>
+          <button onClick={handlePause}>
+            {paused ?
+              <i className="fa-solid fa-play"></i> : 
+              <i className="fa-solid fa-pause"></i>
+            }</button>
+          <button onClick={() => changeMusic('next')}>
+            <i className="fa-solid fa-angle-right"></i>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,46 @@
+// Songs
+import hotelCalifornia from './music/hotel_california.mp3';
+import anotherBrickInTheWall from './music/another_brick_in_the_wall.mp3';
+import yesterday from './music/yesterday.mp3';
+import smokeOnTheWater from './music/smoke_on_the_water.mp3';
+import bohemianRhapsody from './music/bohemian_rhapsody.mp3';
+// Images
+import hotelCaliforniaImg from './assets/hotel_california.jpg';
+import anotherBrickInTheWallImg from './assets/wall.jpg';
+import yesterdayImg from './assets/yesterday.jpg';
+import smokeOnTheWaterImg from './assets/smoke_on_the_water.jpg';
+import bohemianRhapsodyImg from './assets/bohemian_rhapsody.jpg';
+
+
+export const music = [
+  {
+    name: 'Hotel California',
+    author: 'Eagles',
+    img: hotelCaliforniaImg,
+    src: hotelCalifornia,
+  },
+  {
+    name: 'Another Brick in the Wall',
+    author: 'Pink Floyd',
+    img: anotherBrickInTheWallImg,
+    src: anotherBrickInTheWall,
+  },
+  {
+    name: 'Yesterday',
+    author: 'The Beatles',
+    img: yesterdayImg,
+    src: yesterday,
+  },
+  {
+    name: 'Bohemian Rhapsody',
+    author: 'Queen',
+    img: bohemianRhapsodyImg,
+    src: bohemianRhapsody,
+  },
+  {
+    name: 'Smoke On The Water',
+    author: 'Deep Purple',
+    img: smokeOnTheWaterImg,
+    src: smokeOnTheWater,
+  },
+];
